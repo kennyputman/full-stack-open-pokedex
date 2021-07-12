0bd501914e18a080e9136512d72175d2a1e06244
Kenny Putman <kbputman@gmail.com>
2021-07-12 16:13:51 -0500

    set webpack devServer port

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// eslint-disable-next-line no-console
+console.log('PORT: ', PORT)
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
