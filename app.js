fef76f122cb7c44e7fa460fa0137cb8ffd59e8e2
Kenny Putman <kbputman@gmail.com>
2021-07-19 16:39:28 -0500

    add healthcheck endpoints

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(express.static('dist'))
 // eslint-disable-next-line no-console
 console.log('PORT: ', PORT)
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
